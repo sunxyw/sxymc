923b0a798bd367ea31eed5248c4f264f80f07446
sunxyw <xy2496419818@gmail.com>
2020-03-13 13:56:28 +0800

    🎨 格式：增加注释

--- src/Abstracts/CommandAbstract.php
+++ src/Abstracts/CommandAbstract.php
@@ -3,19 +3,55 @@
 namespace Sunxyw\Sxymc\Abstracts;
 
 use Sunxyw\Sxymc\Entities\Invoker;
+use Illuminate\Support\Collection;
 
+/**
+ * 命令抽象类
+ *
+ * @package Sunxyw\Sxymc\Abstracts
+ * @author sunxyw <xy2496419818@gmail>
+ */
 abstract class CommandAbstract
 {
+    /**
+     * 命令名称
+     *
+     * @var string
+     */
     public $name;
+
+    /**
+     * 命令调用者
+     *
+     * @var Invoker
+     */
     public $invoker;
+
+    /**
+     * 命令参数
+     *
+     * @var Collection
+     */
     public $arguments;
 
-    public function __construct($name, Invoker $invoker, $arguments)
+    /**
+     * 实例化命令类
+     *
+     * @param string $name
+     * @param Invoker $invoker
+     * @param Collection $arguments
+     */
+    public function __construct($name, Invoker $invoker, Collection $arguments)
     {
         $this->name = $name;
         $this->invoker = $invoker;
         $this->arguments = $arguments;
     }
 
+    /**
+     * 定义如何执行命令
+     *
+     * @return void
+     */
     abstract public function exec();
 }
--- src/Entities/Command.php
+++ src/Entities/Command.php
@@ -4,8 +4,19 @@ namespace Sunxyw\Sxymc\Entities;
 
 use Sunxyw\Sxymc\Abstracts\CommandAbstract;
 
+/**
+ * 命令实体类
+ *
+ * @package [NAMESPACE]
+ * @author sunxyw <xy2496419818@gmail>
+ */
 class Command extends CommandAbstract
 {
+    /**
+     * 定义如何执行命令
+     *
+     * @return void
+     */
     public function exec()
     {
     }
