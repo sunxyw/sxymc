e6ef8d1edd0015af32eff660b069d736f63a5ca0
sunxyw <xy2496419818@gmail.com>
2020-03-13 14:16:42 +0800

    ✨ 功能：新增注册命令功能

--- src/Handler.php
+++ src/Handler.php
@@ -2,6 +2,9 @@
 
 namespace Sunxyw\Sxymc;
 
+use Illuminate\Support\Collection;
+use Sunxyw\Sxymc\Abstracts\CommandAbstract;
+
 /**
  * 请求处理类
  *
@@ -18,6 +21,13 @@ class Handler
     protected $hash;
 
     /**
+     * 已注册的命令
+     *
+     * @var Collection
+     */
+    protected $commands;
+
+    /**
      * 实例化请求处理类
      *
      * @param string $password 在 WebSend 配置文件处设置的密码
@@ -26,6 +36,24 @@ class Handler
     public function __construct($password, $hash_algorithm = 'sha512')
     {
         $this->hash = hash($hash_algorithm, $password);
+        $this->commands = collect();
+    }
+
+    /**
+     * 注册命令
+     *
+     * @param CommandAbstract $command 命令实体类
+     * @return void
+     */
+    public function register(CommandAbstract $command)
+    {
+        $command_name = $command->name;
+        $exec = $command->exec;
+
+        $this->commands->add([
+            'name' => $command_name,
+            'exec' => $exec
+        ]);
     }
 
     /**
